380b5ad28fc663f350fbb83209d5edd7716692a4
comicalthegreatmodder <91438058+comicalthegreatmodder@users.noreply.github.com>
2024-02-01 15:20:27 +0000

    i commit to update vercel

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div class="progress" ></div>
+        
         <div class="cube-wrap">
           <div class="cube">
             <div class="side top">wsgmaniga</div>
