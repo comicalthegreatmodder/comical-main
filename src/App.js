b55ee89731b338edca13cca0e0cc07a88a4a4d69
comicalthegreatmodder <91438058+comicalthegreatmodder@users.noreply.github.com>
2024-02-16 21:54:16 +0000

    gdf

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {
   Link
 } from "react-router-dom";
 import JP from "./components/JP";
+import CH from "./components/CH";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Routes>
             <Route path="/" element={<p>this is the home page</p>} />
             <Route path="/JP" element={<JP />} />
+            <Route path="/CH" element={<CH />} />
           </Routes>
         </div>
       </BrowserRouter>
