683c02df1256ab9215e549daeb5e9050221db893
comicalthegreatmodder <91438058+comicalthegreatmodder@users.noreply.github.com>
2024-02-20 19:41:18 +0000

    sad

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -7,10 +7,11 @@ function HomePage() {
       <Navbar />
       <h1>Tharun was here - 2/17/24</h1>
       <motion.div
-        initial={{ opacity: 0, scale: 0.5 }}
-        animate={{ opacity: 1, scale: 1 }}
-        transition={{ duration: 0.5 }}
-      />
+        initial={{ opacity:0 }}
+        animate={{ opacity:1 }}
+      >
+        this is the test of framer motion fade-in: 2/20/24
+      </motion.div>
     </>
   )
 }
