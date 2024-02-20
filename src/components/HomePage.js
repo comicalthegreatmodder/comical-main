6b192e8d539189f8d552a6698c1bb615615a0e65
comicalthegreatmodder <91438058+comicalthegreatmodder@users.noreply.github.com>
2024-02-20 19:42:56 +0000

    framer

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -4,12 +4,13 @@ import { motion } from "framer-motion";
 function HomePage() {
   return (
     <>
-      <Navbar />
-      <h1>Tharun was here - 2/17/24</h1>
       <motion.div
         initial={{ opacity:0 }}
         animate={{ opacity:1 }}
       >
+        <Navbar />
+        <h1>Tharun was here - 2/17/24</h1>
+      
         this is the test of framer motion fade-in: 2/20/24
       </motion.div>
     </>
