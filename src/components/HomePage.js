84f8974593c76b4c5a6344e356ca339034a6964b
comicalthegreatmodder <91438058+comicalthegreatmodder@users.noreply.github.com>
2024-02-20 22:02:28 +0000

    k

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -7,7 +7,7 @@ function HomePage() {
     <>
       <motion.div
         initial={{ opacity: 1 }} 
-        transition={{ duration: 2 }}
+        transition={{ duration: 5 }}
         animate= {{ opacity: 0 }}
       >
         <div className="blackBox">
@@ -16,14 +16,14 @@ function HomePage() {
         
       </motion.div>
 
-      <motion.p 
+      <motion.h1 
         initial={{ opacity: 0 }}
-        transition={{ times: [1, 0.2, 0.8] }}
+        transition={{ times: [4, 0.2, 0.8] }}
         animate={{ opacity: [0,1,0] }}
         className="comicalonesign"
       >
         comical.one
-      </motion.p>
+      </motion.h1>
       
       <Navbar />
       <h1>Tharun was here - 2/17/24</h1>
