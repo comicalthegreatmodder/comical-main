1e740193b882da54a4b8fde2a58f089f872747ab
comical <91438058+comicalthegreatmodder@users.noreply.github.com>
2024-02-21 19:55:17 +0000

    this might not work but whatever

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -5,12 +5,8 @@ import InitialTransition from "./InitialTransition"
 function HomePage() {
   return (
     <>
-      <p className="scale-up-center">comical.one</p>
+      <h1 className="flicker-in center-page">comical.one</h1>
       
-      <Navbar />
-      <h1>Tharun was here - 2/17/24</h1>
-      
-      this is the test of framer motion fade-in: 2/20/24
     </>
   )
 }
