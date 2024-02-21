0272fe635fe2912061ba57c7773fffe13399d5d2
comicalthegreatmodder <91438058+comicalthegreatmodder@users.noreply.github.com>
2024-02-21 18:00:44 +0000

    lgtv babyyyy

--- src/components/InitialTransition.js
+++ src/components/InitialTransition.js
@@ -6,7 +6,38 @@ const blackBox = {
     bottom: 0,
   },
   animate: {
-    height: 0,    
+    height: 0,
+    transition: {
+      when: "afterChildren",
+      duration: 1.5,
+      ease: [0.87, 0, 0.13, 1],
+    },
+  },
+};
+
+const textContainer = {
+  initial: {
+    opacity: 1,
+  },
+  animate: {
+    opacity: 0,
+    transition: {
+      duration: 0.25,
+      when: "afterChildren",
+    },
+  },
+};
+
+const text = {
+  initial: {
+    y: 40,
+  },
+  animate: {
+    y: 80,
+    transition: {
+      duration: 1.5,
+      ease: [0.87, 0, 0.13, 1],
+    },
   },
 };
 
@@ -18,7 +49,32 @@ const InitialTransition = () => {
         initial="initial"
         animate="animate"
         variants={blackBox}
-      />      
+      >
+        <motion.svg variants={textContainer} className="">
+          <pattern
+            id="pattern"
+            patternUnits="userSpaceOnUse"
+            width={750}
+            height={800}
+            className="text-white"
+          >
+            <rect className="w-full h-full fill-current" />
+            <motion.rect
+              variants={text}
+              className=""
+            />
+          </pattern>
+          <text
+            className="text-4xl font-bold"
+            text-anchor="middle"
+            x="50%"
+            y="50%"
+            style={{ fill: "url(#pattern)" }}
+          >
+            tailstore
+          </text>
+        </motion.svg>
+      </motion.div>
     </div>
   );
 };
