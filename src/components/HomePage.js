451ec29b12b9b49fa889028a46e25d09c4c517cf
comical <91438058+comicalthegreatmodder@users.noreply.github.com>
2024-02-22 14:19:39 +0000

    i forgot to set the class heeh

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -9,7 +9,7 @@ function HomePage() {
         
       </div>
       
-      <h1 className="flicker-in center-page ">comical.one</h1>
+      <h1 className="flicker-in center-page comicalonetext ">comical.one</h1>
 
       
     </>
