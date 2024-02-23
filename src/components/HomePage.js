6fe337bb31fe933ca5224a0fe57e93c66976e43d
comical <91438058+comicalthegreatmodder@users.noreply.github.com>
2024-02-23 20:32:28 +0000

    lkn

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -13,10 +13,12 @@ function HomePage() {
         <h1 className="comicalonetext play-animations center-page ">comical.one</h1>
 
         <h2 className="slide-in-bck-top">On a mission to make the most beautiful website.</h2>
+        <div>
+          <h3 className="slide-in-bck-left aboutText"><Link to="/about">About</Link></h3>
 
-        <h3 className="slide-in-bck-left aboutText"><Link to="/about">About</Link></h3>
+          <h3 className="slide-in-bck-right testnav2">Test Element</h3>
+        </div>
         
-        <h3 className="slide-in-bck-right testnav2">Test Element</h3>
       </div>
         
     </>
